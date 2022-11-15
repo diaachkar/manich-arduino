20fdd41cdd38d3d1459711b061ff970be6a096c9
Manich <110599345+manich-lb@users.noreply.github.com>
2022-11-15 20:22:11 +0200

    Update manich_final.ino
    small changes

--- manich_final/manich_final.ino
+++ manich_final/manich_final.ino
@@ -1,80 +1,130 @@
-//Motor A
-#define ENA 6
-#define IN1 7
-#define IN2 8
+const int rmotorpin1 = 5;
+const int rmotorpin2 = 6;
 
-//Motor B
-#define IN3 9
-#define IN4 10
-#define ENB 11
+const int lmotorpin1 = 3;
+const int lmotorpin2 = 4;
+const int enA = 2;
+const int enB = 7;
+int count = 0;
+const int frontTriggerPin = 8;
+const int leftTriggerPin = 9;
 
-#define Speed 100
+const int lEchoPin = 13;
+const int fEchoPin = 12;
+const int turningSpeed = 240;
+const int forwardSpeed = 255;
+const int turningDelay = 470;
+long durationLeft = 0;
+long durationRight = 0;
+long durationFront = 0;
+long distanceLeft = 0;
+long distanceRight = 0;
+long distanceFront = 0;
 
 void setup() {
-  //Sensor pins
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  //Motor A
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  //Motor B
-  pinMode(ENB, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+//  Serial.begin(9600);
+  pinMode(lmotorpin1, OUTPUT);
+  pinMode(lmotorpin2, OUTPUT);
+ 
+  pinMode(rmotorpin1, OUTPUT);
+  pinMode(rmotorpin2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(enA, OUTPUT);
 
+  //ultrasonic sensor pins.
+  pinMode(frontTriggerPin, OUTPUT);
+  pinMode(leftTriggerPin, OUTPUT);
+  pinMode(lEchoPin, INPUT);
+  pinMode(fEchoPin, INPUT);
 }
+
 void loop() {
-  bool SOne = digitalRead(2);
-  bool STwo = digitalRead(3);
-  bool SThree = digitalRead(4);
-  bool SFour = digitalRead(5);
+  distanceFront = getDistance("front");
+ 
+//  Serial.println(distanceLeft);
+//  Serial.println(distanceRight);
+//  Serial.println(distanceFront);
 
-  if (SOne == 0 && STwo == 1 && SThree == 1  ) {
-    forward();
-  } else if (SOne == 1 && STwo == 1 && SThree == 1) {
-    stopAll();
-  } else if (SOne == 0 && STwo == 0 && SThree == 0) {
-    stopAll();
-  }  else if (SOne == 1 && STwo == 1 && SThree == 0) {
-    turnLeft();
-  } else if (SOne == 0 && STwo == 0 && SThree == 1) {
-    turnRight();
+  if(distanceFront > 20){
+    moveForward();
+  } else {
+    distanceLeft = getDistance("left");
+    count += 1;
+    if(distanceLeft < 50){
+      turnRight();
+    }
+    else {
+      turnLeft();
+    }
+     
   }
+  if(count == 12){
+    moveForward();
+    delay(200);
+    stopMoving();
+    exit(0);
+  }
+   
+ 
+//  turnRight();
+//  stopMoving();
+//  exit(0);
+  //delay(500);
 }
 
-void forward() {
-  analogWrite(ENA, Speed);
-  analogWrite(ENB, Speed);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-void turnRight() {
-  analogWrite(ENA, Speed);
-  analogWrite(ENB, Speed);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-void turnLeft() {
-  analogWrite(ENA, Speed);
-  analogWrite(ENB, Speed);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
 }
 
-void stopAll() {
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+void moveForward(){
+  analogWrite(enA, forwardSpeed);
+  analogWrite(enB, forwardSpeed);
+  digitalWrite(rmotorpin1, HIGH);
+  digitalWrite(rmotorpin2, LOW);
+  digitalWrite(lmotorpin1, HIGH);
+  digitalWrite(lmotorpin2, LOW);
+}
+
+void stopMoving(){
+  digitalWrite(rmotorpin1, LOW);
+  digitalWrite(rmotorpin2, LOW);
+  digitalWrite(lmotorpin1, LOW);
+  digitalWrite(lmotorpin2, LOW);
+}
+
+void turnRight(){
+  analogWrite(enA, turningSpeed);
+  analogWrite(enB, turningSpeed);
+  digitalWrite(rmotorpin1, LOW);
+  digitalWrite(rmotorpin2, HIGH);
+  digitalWrite(lmotorpin1, HIGH);
+  digitalWrite(lmotorpin2, LOW);
+  delay(turningDelay);
+}
+
+void turnLeft(){
+  analogWrite(enA, turningSpeed);
+  analogWrite(enB, turningSpeed);
+  digitalWrite(rmotorpin1, HIGH);
+  digitalWrite(rmotorpin2, LOW);
+  digitalWrite(lmotorpin1, LOW);
+  digitalWrite(lmotorpin2, HIGH);
+  delay(turningDelay);
+}
+
+long getDistance(String direction){
+  int pin = fEchoPin;
+  int triggerPin = frontTriggerPin;
+  if(direction == "left"){
+    pin = lEchoPin;
+    triggerPin = leftTriggerPin;
+  }
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(40);
+  digitalWrite(triggerPin, LOW);
+  long duration = pulseIn(pin, HIGH);
+  long distance = microsecondsToCentimeters(duration);
+  return distance;
 }
